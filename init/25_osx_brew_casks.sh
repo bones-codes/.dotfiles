1bb779c192911abc3e11d728f3fb8a002b5f9633
cara marie <cara@me>
2015-11-28 09:24:22 -0800

    ricochet

--- init/25_osx_brew_casks.sh
+++ init/25_osx_brew_casks.sh
@@ -18,7 +18,7 @@ if [[ $HACK || $REV ]]; then
 fi
 
 if [ "$LOCAL" ]; then
-  casks+=(1password vagrant mactex tunnelblick vmware-fusion little-snitch)
+  casks+=(ricochet 1password vagrant mactex tunnelblick vmware-fusion little-snitch)
 fi
 
 # Install Homebrew casks.
