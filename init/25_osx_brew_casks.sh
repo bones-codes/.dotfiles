b4367fb47e3b407aef687c4fcda25d4dfe34c834
cara marie <cara@me>
2015-12-08 23:45:46 -0800

    you don't need knox when you have the cli

--- init/25_osx_brew_casks.sh
+++ init/25_osx_brew_casks.sh
@@ -18,7 +18,7 @@ if [[ $HACK || $REV ]]; then
 fi
 
 if [ "$LOCAL" ]; then
-  casks+=(ricochet 1password vagrant mactex vmware-fusion little-snitch knox)
+  casks+=(ricochet 1password vagrant mactex vmware-fusion little-snitch)
 fi
 
 # Install Homebrew casks.
