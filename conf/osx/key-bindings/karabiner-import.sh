0e56912a9de1771105f93ee30f870b161e81a4df
cara marie <bones@codes>
2016-03-06 02:06:13 -0500

    another karabiner update

--- conf/osx/key-bindings/karabiner-import.sh
+++ conf/osx/key-bindings/karabiner-import.sh
@@ -2,10 +2,6 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.wait 0
-/bin/echo -n .
-$cli set repeat.initial_wait 300
-/bin/echo -n .
 $cli set remap.hjkl_arrow 1
 /bin/echo -n .
 $cli set remap.hjkl_arrow_xcode 1
