59660319b8c6161d974ced2cfc769c5aad5d3116
cara marie <bones@codes>
2016-03-06 01:56:16 -0500

    fixed karabiner and seil imports

--- init/30_osx.sh
+++ init/30_osx.sh
@@ -66,10 +66,12 @@ fi
 e_header "Installing Karabiner and Seil sets"
 sudo -u $STANDARD_USER open -a karabiner
 sleep 10
-sudo -u $STANDARD_USER pkill Karabiner
 sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" 'UPDATE access SET allowed = "1";'
 sh $DOTFILES_HOME/conf/osx/key-bindings/karabiner-import.sh
+sudo -u $STANDARD_USER pkill Karabiner
+sudo -u $STANDARD_USER open -a Seil
 sh $DOTFILES_HOME/conf/osx/key-bindings/seil-import.sh
+sudo -u $STANDARD_USER pkill Seil
 
 if [[ "$(type -P pip)" ]]; then
   e_header "Install and/or Upgrade PIP"
