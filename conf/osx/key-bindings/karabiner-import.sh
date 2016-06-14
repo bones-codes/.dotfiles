651edd58021d96c80c3c9bad3fa08a76d83e5bdb
cara marie <bones@codes>
2016-06-14 12:47:51 -0400

    no longer syncing remotely and other updates

--- conf/osx/key-bindings/karabiner-import.sh
+++ conf/osx/key-bindings/karabiner-import.sh
@@ -2,12 +2,12 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.wait 20
-/bin/echo -n .
-$cli set repeat.initial_wait 500
-/bin/echo -n .
 $cli set remap.hjkl_arrow 1
 /bin/echo -n .
 $cli set remap.hjkl_arrow_xcode 1
+/bin/echo -n .
+$cli set repeat.initial_wait 500
+/bin/echo -n .
+$cli set repeat.wait 20
 /bin/echo -n .
 /bin/echo
